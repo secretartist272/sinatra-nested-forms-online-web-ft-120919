c8cc08ba4b796c48d088de4aa785f440a3618159
Jacqueline Freeland <secretartist272@gmail.com>
2020-02-08 03:14:05 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -2,18 +2,15 @@ require './environment'
 
 module FormsLab
   class App < Sinatra::Base
-
-    # code other routes/actions here
-    
     get '/' do
-      erb:index
+      erb :root
     end
-    
+
     get '/new' do
-      erb :pirates/new
+      erb :'pirates/new'
     end
-    
-     post '/pirates' do
+
+    post '/pirates' do
       @pirate = Pirate.new(params[:pirate])
 
       params[:pirate][:ships].each do |details|
@@ -23,6 +20,6 @@ module FormsLab
 
       erb :'pirates/show'
     end
-
+    
   end
 end
